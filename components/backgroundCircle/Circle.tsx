b6372813727b6605fc6281c6a90c473753de7b04
Bryan Magistrado <bmagistrado@yahoo.com>
2025-02-17 21:10:07 +0000

    Installed GSAP, added background component

---
+++ components/backgroundCircle/Circle.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./circle.css";
+
+const Circle = () => {
+  return (
+    <div className="background">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default Circle;
